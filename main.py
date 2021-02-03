4abea869b87cf174d527ba704ee3121814be2036
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-02-02 17:17:47 -0800

    Center window on start up

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ class FileUploader(QWidget):
         super().__init__()
         self.setGeometry(200, 200, 500, 500)
         self.setWindowTitle("File Uploader")
-
+        self.center()
+        
         self.initUI()
 
     def initUI(self):
         layout = QHBoxLayout()
         layout.addWidget(QLabel("AAAAAAAAAA"))
         self.setLayout(layout)
+
+    def center(self):
+        qr = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(centerPoint)
+        self.move(qr.topLeft())
 
 app = QApplication([])
 with open("stylesheet.qss", "r") as fh:

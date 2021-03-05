63afc0dd96728c2c177c030f8dff4447945b06e7
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-03-05 06:31:07 -0800

    Upload to ddb

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from PyQt5.QtWidgets import *
 
 class FileUploader(QWidget):
 
-    def __init__(self, s3):
+    def __init__(self, s3, table):
         super().__init__()
         self.s3 = s3
+        self.table = table
         self.setGeometry(200, 200, 300, 200)
         self.setWindowTitle("File Uploader")
         self.center()
@@ -16,25 +17,34 @@ class FileUploader(QWidget):
         self.initUI()
 
     def initUI(self):
-        layout = QHBoxLayout()
-        layout.addSpacing(10)
+        overallLayout = QVBoxLayout()
+        uploadLayout = QHBoxLayout()
+        uploadLayout.addSpacing(10)
         
         self.uploadMp3Button = QPushButton("Upload .mp3")
         self.uploadMp3Button.clicked.connect(self.uploadMp3)
-        layout.addWidget(self.uploadMp3Button, 1)
-        layout.addSpacing(10)
+        uploadLayout.addWidget(self.uploadMp3Button, 1)
+        uploadLayout.addSpacing(10)
 
         self.uploadAlbumButton = QPushButton("Upload Album")
         self.uploadAlbumButton.clicked.connect(self.uploadAlbum)
-        layout.addWidget(self.uploadAlbumButton, 1)
-        layout.addSpacing(10)
+        uploadLayout.addWidget(self.uploadAlbumButton, 1)
+        uploadLayout.addSpacing(10)
         
         self.uploadArtistButton = QPushButton("Upload Artist")
         self.uploadArtistButton.clicked.connect(self.uploadArtist)
-        layout.addWidget(self.uploadArtistButton, 1)
-        layout.addSpacing(10)
+        uploadLayout.addWidget(self.uploadArtistButton, 1)
+        uploadLayout.addSpacing(10)
+        overallLayout.addLayout(uploadLayout)
+        genreLayout = QHBoxLayout()
+        genreLayout.addStretch(1)
+        self.genreBox = QLineEdit()
+        genreLayout.addWidget(QLabel("Genre:"))
+        genreLayout.addWidget(self.genreBox)
+        genreLayout.addStretch(1)
+        overallLayout.addLayout(genreLayout)
         
-        self.setLayout(layout)
+        self.setLayout(overallLayout)
 
     def center(self):
         qr = self.frameGeometry()
@@ -74,6 +84,17 @@ class FileUploader(QWidget):
                 if song.split('.')[-1] != 'mp3': continue
                 data = open(fp, 'rb')
                 key = artist + '/' + album + '/' + song
+                table_item = {
+                    'genre': self.genreBox.text(),
+                    'artist_album_song': artist + '_' + album + '_' + song.split('.mp3')[0],
+                    'artist': artist,
+                    'album': album,
+                    'song': song.split('.mp3')[0],
+                    'info': {
+                        's3_key': key
+                    }
+                }
+                table.put_item(Item=table_item)
                 s3.Bucket('cs493-gunnellg-bucket').put_object(Key=key, Body=data)
                 data.close()
 
@@ -82,9 +103,10 @@ app = QApplication([])
 session = boto3.session.Session()
 s3 = session.resource('s3')
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+table = dynamodb.Table('music')
 with open("stylesheet.qss", "r") as fh:
     app.setStyleSheet(fh.read())
-fileUploader = FileUploader(s3)
+fileUploader = FileUploader(s3, table)
 fileUploader.show()
 
 sys.exit(app.exec_())
